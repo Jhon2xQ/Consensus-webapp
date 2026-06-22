f9098246fb1688d8b6f5892eb645ea721e582e26
Jhon2xQ <jhonq2512@gmail.com>
2026-06-22 16:12:17 -0500

    fix: callback

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,9 +2,9 @@ import type { Actions } from "./$types";
 
 export const actions = {
   signIn: async ({ locals }) => {
-    await locals.logtoClient.signIn("https://consensus.carmenio.com/callback/");
+    await locals.logtoClient.signIn("https://consensus.carmenio.com/callback");
   },
   signOut: async ({ locals }) => {
-    await locals.logtoClient.signOut("https://consensus.carmenio.com/");
+    await locals.logtoClient.signOut("https://consensus.carmenio.com");
   },
 } satisfies Actions;
